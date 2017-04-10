6ebce85bace38c4cb29fde8efd7a7c955999612c
Erica Rodriguez <erica.rodriguez.mail@gmail.com>
2017-04-10 01:01:17 -0500

    last try

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,21 +1,55 @@
 
+    var timer = setInterval(function() {
 
-//  Interval Demonstration
-    //  Set our number counter to 30.
-    var number = 30;
+        myTimer()
 
-    //  Variable that will hold our interval ID when we execute
-    //  the "run" function
-    var intervalId;
+    }, 1000);
 
-    //  When the stop button gets clicked, run the stop function.
-    //$("#stop").on("click", stop);
+        var seconds = 30;
 
-    //  When the start button gets clicked, execute the run function.
-    $("#start").on("click", run);
+    function myTimer() {
 
-    //  The run function sets an interval
-    //  that runs the decrement function once a second.
+        document.getElementById(".timer-box").innerHTML = "0:" + seconds--; + "seconds";
+
+    }
+
+//sets up our score keeping information 
+		var correct = 0;
+		var incorrect = 0;
+		var unanswered = 0;
+		var number = 20;
+		var intervalId;
+
+		function showScore (){
+			$("#correct").text(correct);
+			$("#incorrect").text(incorrect);
+			$("#unanswered").text(unanswered);
+}
+
+//hides the questions until the start button is clicked
+		$(".questions").hide();
+		$(".result").hide();
+
+
+		$(".start").on("click", showQuestions);
+		$(".submit").on("click", showScore);
+
+//showQuestions function shows the game section and hides the start button
+function showQuestions () {
+	$(".questions").show();
+	$(".start").hide();
+	run();
+}
+//showScore function: hides the game section and shows the result section.
+function showScore () {
+	$(".questions").hide();
+	$(".result").show();
+	showScore();
+}
+
+$(".submit").on("click", stop);
+    $(".submit").on("click", checkAnswers);
+
     function run() {
       intervalId = setInterval(decrement, 1000);
     }
@@ -26,28 +60,155 @@
       //  Decrease number by one.
       number--;
 
-      //  Show the number in the timer-box div
-      $("#timer-box").html("<h2>" + number + "</h2>");
-
+      //  Show the number in the timer-box.
+      $("#timer-box").text(number);
 
       //  Once number hits zero...
       if (number === 0) {
-
-        //  ...run the stop function.
-        stop();
-
-        
+      	//  ...run the showScore function.
+        showScore();
       }
     }
+
 
     //  The stop function
     function stop() {
 
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
       clearInterval(intervalId);
+      showScore();
     }
 
-    //  Execute the run function.
-    run();
+    //this function checks the correct, incorrect and unanswered questions.
+function checkAnswers(){
+	
+	function question1 (){
+	    if ($('input[name="q1"]:checked').val() == "pepper-mustard"){
+    	correct++;
+	    }
+	    else if ($('input[name="q1"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question1);
+	}
+
+	function question2 (){
+	    if ($('input[name="q2"]:checked').val() == "shredded-wheat"){
+    	correct++;
+	    }
+	    else if ($('input[name="q2"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question2);
+	}
+
+	function question2 (){
+	    if ($('input[name="q2"]:checked').val() == "shredded-wheat"){
+    	correct++;
+	    }
+	    else if ($('input[name="q2"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question2);
+	}
+
+	function question3 (){
+	    if ($('input[name="q3"]:checked').val() == "cabbage"){
+    	correct++;
+	    }
+	    else if ($('input[name="q3"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question3);
+	}
+
+	function question4 (){
+	    if ($('input[name="q4"]:checked').val() == "fried-chicken"){
+    	correct++;
+	    }
+	    else if ($('input[name="q4"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question4);
+	}
+
+	function question5 (){
+	    if ($('input[name="q5"]:checked').val() == "ireland"){
+    	correct++;
+	    }
+	    else if ($('input[name="q5"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question5);
+	}
+
+	function question6 (){
+	    if ($('input[name="q6"]:checked').val() == "cheerioates"){
+    	correct++;
+	    }
+	    else if ($('input[name="q6"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question6);
+	}
+
+	function question7 (){
+	    if ($('input[name="q7"]:checked').val() == "quesadilla"){
+    	correct++;
+	    }
+	    else if ($('input[name="q7"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question7);
+	}
+
+	function question8 (){
+	    if ($('input[name="q8"]:checked').val() == "coca-cola"){
+    	correct++;
+	    }
+	    else if ($('input[name="q8"]:checked').val()== undefined){
+	    	unanswered++;
+	    }
+	    else {
+	    	incorrect++;
+	    }
+	console.log(question8);
+	}
+
+	question1();
+	question2();
+	question3();
+	console.log("Correct answers: " + correct);
+	console.log("Incorrect answers: "+ incorrect);
+	console.log("Unanswered answers: " + unanswered);
+	$("#correct").text(correct);
+	$("#incorrect").text(incorrect);
+	$("#unanswered").text(unanswered);
+    
+
+
+
+
